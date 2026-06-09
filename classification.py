fea36f63df501d7e62c803d84242764f47ef4750
Ubuntu <s2410929006@fhooe.at>
2026-06-09 20:23:51 +0000

    tried improving model

--- classification.py
+++ classification.py
@@ -14,10 +14,12 @@ from pathlib import Path
 import numpy as np
 import tensorflow as tf
 import keras
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, balanced_accuracy_score, classification_report
+from sklearn.utils.class_weight import compute_class_weight
 
-from keras.layers import Conv2D, Flatten, Dense, GlobalAveragePooling2D, Dropout
+from keras.layers import Conv2D, Flatten, Dense, GlobalAveragePooling2D, GlobalMaxPooling2D, Dropout
 from keras.models import Model
+from keras.applications.efficientnet_v2 import preprocess_input
 
 def load_img(image_path, label_path): 
     img = Image.open(image_path).convert("L")  # convert to grayscale
@@ -29,6 +31,11 @@ def load_img(image_path, label_path):
     with open(label_path) as f:
         for annotation in f:
             cls, xc, yc, w, h, _ = map(float, annotation.split()) # convert them to float, no need to import motion
+
+            # exclude samples who are less than 5 pixels wide or high -> too small
+            if w * W < 5 or h * H < 5:
+                continue
+
             x1 = int((xc - w/2) * W)
             y1 = int((yc - h/2) * H)
             x2 = int((xc + w/2) * W)
@@ -46,18 +53,18 @@ def load_dataset(image_path="images_filtered/train", label_path="labels_filtered
 
     X = []
     Y = []
-
-    for img, label in zip(sorted(images_dir.iterdir()), sorted(labels_dir.iterdir())):
-        x, y = load_img(img, label)
-        X.extend(x)
-        Y.extend(y)
+    for img_path in images_dir.glob("*"):
+        lbl_path = labels_dir / f"{img_path.stem}.txt" 
+        
+        if lbl_path.exists():
+            x, y = load_img(img_path, lbl_path)
+            X.extend(x)
+            Y.extend(y)
     
     return X, Y
 
 # resize bc for training they all need to have the same size
 def resize_img(X_train, X_val, X_test, size=(128, 128)):
-    X = X_train + X_val + X_test
-
     # convert to correct input format for tensorflow
     x_train = np.array([np.array(img.resize(size)) for img in X_train], dtype=np.float32)
     x_val = np.array([np.array(img.resize(size)) for img in X_val], dtype=np.float32)
@@ -65,22 +72,61 @@ def resize_img(X_train, X_val, X_test, size=(128, 128)):
 
     return x_train, x_val, x_test
 
-# do i want to do that?!
-def augment_data():
-    pass
+def resize_img_padding(X_train, X_val, X_test, size=128): 
+    x_train = np.array([np.array(resize_with_padding(img, size)) for img in X_train], dtype=np.float32)
+    x_val = np.array([np.array(resize_with_padding(img, size)) for img in X_val], dtype=np.float32)
+    x_test = np.array([np.array(resize_with_padding(img, size)) for img in X_test], dtype=np.float32)
+
+    return x_train, x_val, x_test
+
+def resize_with_padding(img, size=128):
+    w, h = img.size
+    if w == 0 or h == 0:
+        print("BAD IMAGE:", img.size)
+
+    scale = size / max(w, h)
+    new_w = int(w * scale)
+    new_h = int(h * scale)
+    img = img.resize((new_w, new_h))
+
+    canvas = Image.new("L", (size, size), 0)
+    x = (size - new_w) // 2
+    y = (size - new_h) // 2
+    canvas.paste(img, (x, y))
+
+    return canvas
+
+def get_class_weights(y): 
+    classes = np.unique(y.numpy())
+
+    weights = compute_class_weight(
+        class_weight="balanced",
+        classes=classes,
+        y=y.numpy()
+    )
+
+    return dict(zip(classes, weights))
 
 def make_model(width, height) -> tf.keras.Model:
+    data_augmentation = tf.keras.Sequential([
+        tf.keras.layers.RandomFlip("horizontal"),
+        tf.keras.layers.RandomRotation(0.05),
+        tf.keras.layers.RandomZoom(0.1),
+    ])
+
     model = tf.keras.Sequential(
         [
             tf.keras.layers.Input(shape=(height, width, 1)),
+            data_augmentation,
             tf.keras.layers.Conv2D(64, 3, activation="relu"), 
             tf.keras.layers.MaxPooling2D(),
-            tf.keras.layers.Conv2D(64, 3, activation="relu"),
-            tf.keras.layers.MaxPooling2D(),
             tf.keras.layers.Conv2D(128, 3, activation="relu"),
-            tf.keras.layers.GlobalMaxPooling2D(),
-            tf.keras.layers.Dense(64, activation="relu"),
-            tf.keras.layers.Dense(32),
+            tf.keras.layers.MaxPooling2D(),
+            tf.keras.layers.Conv2D(256, 3, activation="relu"),
+            tf.keras.layers.MaxPooling2D(),
+            tf.keras.layers.Flatten(),
+            tf.keras.layers.Dense(256, activation="relu"),
+            tf.keras.layers.Dropout(0.3),
             tf.keras.layers.Dense(5, activation="softmax"),
         ]
     )
@@ -100,52 +146,73 @@ def make_transfer_model(input_shape=(64, 64, 3)):
         pooling=None
     )
 
-    base_model.trainable = True
+    base_model.trainable = False
+    data_augmentation = keras.Sequential([
+        keras.layers.RandomFlip("horizontal"),
+        keras.layers.RandomRotation(0.05),
+        keras.layers.RandomZoom(0.1),
+    ])
     inputs = keras.Input(shape=input_shape)
-    x = base_model(inputs, training=True)  
-    x = Flatten()(x)
-    x = Dense(64, activation='relu')(x)
-    x = Dense(32, activation='relu')(x)
-    x = Dropout(0.3)(x)
+    x = data_augmentation(inputs)
+    x = base_model(x, training=False)  
+    x = GlobalAveragePooling2D()(x)
+    x = Dense(512, activation='relu')(x)
+    x = Dropout(0.4)(x)
+    x = Dense(256, activation='relu')(x)
+    x = Dropout(0.4)(x)
     outputs = Dense(5, activation='softmax')(x)
     model = Model(inputs=inputs, outputs=outputs)
     model.compile(
-            optimizer=keras.optimizers.Adam(),
+            optimizer=keras.optimizers.Adam(1e-3),
             loss="sparse_categorical_crossentropy",
             metrics=["accuracy"]
         )
     return model
-
-def evaluate(X_test, y_test):
-    pass
 
 if __name__ == "__main__":
     X_train, y_train = load_dataset()
     X_val, y_val = load_dataset("images_filtered/val", "labels_filtered/val")
     X_test, y_test = load_dataset("images_filtered/test", "labels_filtered/test")
     print("images loaded")
-    X_train, X_val, X_test = resize_img(X_train, X_val, X_test)
+    X_train, X_val, X_test = resize_img_padding(X_train, X_val, X_test, 164)
     print("images resized")
     # need additional dim for keras input
     X_train = X_train[..., np.newaxis]
     X_val = X_val[..., np.newaxis]
     X_test = X_test[..., np.newaxis]
 
-    X_train /= 255.0
-    X_val /= 255.0
-    X_test /= 255.0
+    # convert to rgb format for transfer learning
+    X_train = np.repeat(X_train, 3, axis=-1)
+    X_val   = np.repeat(X_val, 3, axis=-1)
+    X_test  = np.repeat(X_test, 3, axis=-1)
+    
+    X_train = preprocess_input(X_train)
+    X_val = preprocess_input(X_val)
+    X_test = preprocess_input(X_test)
+
+    #X_train /= 255.0
+    #X_val /= 255.0
+    #X_test /= 255.0
 
     y_train = tf.convert_to_tensor(y_train, dtype=tf.int32)
     y_val = tf.convert_to_tensor(y_val, dtype=tf.int32)
     y_test = tf.convert_to_tensor(y_test, dtype=tf.int32)
 
-    # convert to rgb format for transfer learning
-    X_train = np.repeat(X_train, 3, axis=-1)
-    X_val   = np.repeat(X_val, 3, axis=-1)
-    X_test  = np.repeat(X_test, 3, axis=-1)
+    print(np.unique(y_val.numpy(), return_counts=True))
+    print(np.unique(y_test.numpy(), return_counts=True))
 
     print(X_train.shape)
     print(np.unique_counts(y_train))
+
+    # imbalanced classes 
+    class_weights = get_class_weights(y_train)
+    print(class_weights)
+
+    early_stop = keras.callbacks.EarlyStopping(
+        monitor="val_accuracy",
+        patience=5,
+        restore_best_weights=True
+    )
 
     #model = make_model(X_train.shape[1], X_train.shape[2])
     model = make_transfer_model((X_train.shape[1], X_train.shape[2], X_train.shape[3]))
@@ -155,8 +222,10 @@ if __name__ == "__main__":
         validation_data=(X_val, y_val),
         epochs=20,
         batch_size=32,
+        class_weight=class_weights,
         verbose=2,
         shuffle=True,
+        callbacks=[early_stop]
     )
 
     print(model.summary())
@@ -164,8 +233,14 @@ if __name__ == "__main__":
     test_loss, test_acc = model.evaluate(X_test, y_test)
     print("Test loss:", test_loss)
     print("Test accuracy:", test_acc)
+
     # print a confusion matrix
     y_pred = model.predict(X_test)
     y_pred_classes = np.argmax(y_pred, axis=1)
     cm = confusion_matrix(y_test, y_pred_classes)
     print(cm)
+
+    balanced_acc = balanced_accuracy_score(y_test, y_pred_classes)
+    print("Balanced accuracy:", balanced_acc)
+    print(classification_report(y_test, y_pred_classes, digits=3))
+    model.save("classification_animals_model.keras")
